45287d41587b57a67b0dfc8ea2482a778a1d67bb
S. Stegu <sashastegu@gmail.com>
2019-01-18 14:54:01 -0500

    add myservice project

---
+++ MyService/CustomAuthorization.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Claims;
+using System.IdentityModel.Policy;
+using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Principal;
+using System.Web;
+
+namespace MyService
+{
+    // based on https://leastprivilege.com/2008/03/19/using-identitymodel-adding-asp-net-support-part-1-authentication-based-claims/
+    public class CustomAuthorizationPolicy : IAuthorizationPolicy
+    {
+
+        public CustomAuthorizationPolicy()
+        {
+            this.Id = Guid.NewGuid().ToString();
+        }
+
+        public ClaimSet Issuer
+        {
+            get { return ClaimSet.System; }
+        }
+
+        public string Id { get; }
+
+        public bool Evaluate(EvaluationContext evaluationContext, ref object state)
+        {
+            try
+            {             
+
+                X509Certificate2 cert = ((X509CertificateClaimSet)evaluationContext.ClaimSets[0]).X509Certificate;
+                string thumbPrint = cert.Thumbprint;
+                string acct = System.Configuration.ConfigurationManager.AppSettings[thumbPrint];
+
+
+                using (var winId = new System.Security.Principal.WindowsIdentity(acct))
+                {
+
+                    using (var wClaimSet = new WindowsClaimSet(winId))
+                    {
+                        evaluationContext.Properties["Principal"] = new WindowsPrincipal(winId);
+
+                        evaluationContext.AddClaimSet(this, wClaimSet);
+                    }
+
+                }
+
+
+                return true;
+            }
+            catch (Exception e)
+            {
+                Console.Write(e.Message);
+                return false;
+            }
+
+        }
+
+
+    }
+}
---
+++ MyService/Service1.svc.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Security.Permissions;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace MyService
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        static class PermissionHelper
+        {
+
+            public static void CheckPermission(string grp)
+            {
+
+                // We get the allowed windows account who is authorized to call this WCF method from configuration file
+
+                PrincipalPermission ppAdmin = new PrincipalPermission(null, grp);
+                ppAdmin.Demand();
+
+            }
+        }
+
+        [PrincipalPermission(SecurityAction.Demand, Role = "ServiceGroup")]
+        public string GetData(int value)
+        {
+            PermissionHelper.CheckPermission(System.Configuration.ConfigurationManager.AppSettings["GetDataGroup"]);
+
+            return string.Format("You entered: {0}", value);
+        }
+
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
